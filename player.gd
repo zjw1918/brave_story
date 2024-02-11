b610bb00a9f399bb21fc800019d982fab4af63a7
zjw1918 <zjw1918@sina.cn>
2024-02-11 22:36:37 +0800

    lesson 05: state machine

--- player.gd
+++ player.gd
@@ -1,5 +1,14 @@
 extends CharacterBody2D
 
+enum State {
+	IDLE,
+	RUNNING,
+	JUMP,
+	FALL,
+	LANDING,
+}
+
+const GROUND_STATE := [State.IDLE, State.RUNNING, State.LANDING]
 const RUN_SPEED := 160.0
 const ACCELERATION_FLOOR := RUN_SPEED / 0.2
 # the acceleration in the air is faster than that in the floor
@@ -7,7 +16,8 @@ const ACCELERATION_FLOOR := RUN_SPEED / 0.2
 const ACCELERATION_AIR := RUN_SPEED / 0.02
 const JUNP_VELOCITY := -320
 
-var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
+var default_gravity := ProjectSettings.get("physics/2d/default_gravity") as float
+var is_first_tick := false
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -18,43 +28,97 @@ var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
 		jump_request_timer.start()
-	if event.is_action_released("jump") and velocity.y < JUNP_VELOCITY / 2:
-		velocity.y = JUNP_VELOCITY / 2
+	if event.is_action_released("jump"):
+		jump_request_timer.stop()
+		if velocity.y < JUNP_VELOCITY / 2:
+			velocity.y = JUNP_VELOCITY / 2
 
-func _physics_process(delta: float) -> void:
+func tick_physics(state: State, delta: float) -> void:
+	match state:
+		State.IDLE:
+			move(default_gravity, delta)
+		State.RUNNING:
+			move(default_gravity, delta)
+		State.JUMP:
+			move(0.0 if is_first_tick else default_gravity, delta)
+		State.FALL:
+			move(default_gravity, delta)
+		State.LANDING:
+			stand(delta)
+	is_first_tick = false
+	
+func move(gravity: float, delta: float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
 	var acceleration := ACCELERATION_FLOOR if is_on_floor() else ACCELERATION_AIR
 	velocity.x = move_toward(velocity.x, direction * RUN_SPEED, acceleration * delta)
 	velocity.y += gravity * delta
 	
-	var can_jump := is_on_floor() or coyote_timer.time_left > 0
-	var should_jump := can_jump and jump_request_timer.time_left > 0
-	if should_jump:
-		velocity.y = JUNP_VELOCITY
-		coyote_timer.stop()
-		jump_request_timer.stop()
-	
-	if is_on_floor():
-		if is_zero_approx(direction) and is_zero_approx(velocity.x):
-			animation_player.play("idle")
-		else:
-			animation_player.play("running")
-	else:
-		animation_player.play("jump")
-
 	# horizontal flip player
 	if not is_zero_approx(direction):
 		sprite_2d.flip_h = direction < 0
-	
-	var was_on_floor := is_on_floor()
-	
+
 	move_and_slide()
 	
-	# handle coyote time jump
-	if is_on_floor() != was_on_floor:
-		# 原先在地面上的，并且不是因为起跳离开的地面
-		if was_on_floor and not should_jump:
-			coyote_timer.start()
-		else:
-			coyote_timer.stop()
+func stand(delta: float) -> void:
+	var acceleration := ACCELERATION_FLOOR if is_on_floor() else ACCELERATION_AIR
+	velocity.x = move_toward(velocity.x, 0.0, acceleration * delta)
+	velocity.y += default_gravity * delta
+	move_and_slide()
 	
+
+func get_next_state(state: State) -> State:
+	var can_jump := is_on_floor() or coyote_timer.time_left > 0
+	var should_jump := can_jump and jump_request_timer.time_left > 0
+	if should_jump:
+		return State.JUMP
+	
+	var direction := Input.get_axis("move_left", "move_right")
+	var is_still := is_zero_approx(direction) and is_zero_approx(velocity.x)
+	
+	match state:
+		State.IDLE:
+			if not is_on_floor():
+				return State.FALL
+			if not is_still:
+				return State.RUNNING
+
+		State.RUNNING:
+			if not is_on_floor():
+				return State.FALL
+			if is_still:
+				return State.IDLE
+				
+		State.JUMP:
+			if velocity.y >= 0:
+				return State.FALL
+		State.FALL:
+			if is_on_floor():
+				return State.LANDING if is_still else State.RUNNING
+		State.LANDING:
+			if not animation_player.is_playing():
+				return State.IDLE
+	return state
+
+func transition_state(from: State, to: State) -> void:
+	if from not in GROUND_STATE and to in GROUND_STATE:
+		coyote_timer.stop()
+	
+	match to:
+		State.IDLE:
+			animation_player.play("idle")
+		State.RUNNING:
+			animation_player.play("running")
+		State.JUMP:
+			animation_player.play("jump")
+			velocity.y = JUNP_VELOCITY
+			coyote_timer.stop()
+			jump_request_timer.stop()
+		State.FALL:
+			animation_player.play("fall")
+			if from in GROUND_STATE:
+				coyote_timer.start()
+		State.LANDING:
+			animation_player.play("landing")
+	is_first_tick = true
+			
+
